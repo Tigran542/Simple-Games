b303d4352cfb881bd958c7276009154bb8539e04
Tigran542 <tikogrig7@gmail.com>
2023-08-18 16:41:45 +0400

    public transform

--- Assets/Scripts/Basics.cs
+++ Assets/Scripts/Basics.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Basics : MonoBehaviour
 {
 
     public GameObject obj;
+    public Transform pos;
+    public BoxCollider Box;
 
     private void Start()
     {
-        obj.SetActive(false);
+        // obj.SetActive(false);
+        // obj.GetComponent<Transform>().position = new Vector3(10, 0, 5);
+        pos.position = new Vector3(10, 5, 7);
     }
 }
