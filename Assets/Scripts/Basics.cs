62fd623a03098c383861299ebe762196b8d06af2
Tigran542 <tikogrig7@gmail.com>
2023-08-18 16:51:00 +0400

    GameObject[ ]

--- Assets/Scripts/Basics.cs
+++ Assets/Scripts/Basics.cs
@@ -4,14 +4,17 @@ using UnityEngine.PlayerLoop;
 public class Basics : MonoBehaviour
 {
 
-    public GameObject obj;
+    public GameObject[] objs = new GameObject[3];
     public Transform pos;
     public BoxCollider Box;
+    public Light light1;
+
 
     private void Start()
     {
         // obj.SetActive(false);
         // obj.GetComponent<Transform>().position = new Vector3(10, 0, 5);
         pos.position = new Vector3(10, 5, 7);
+        light1.intensity = 0.5f;
     }
 }
