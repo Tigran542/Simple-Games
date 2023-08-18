af00dee23cd1eea5adb1aa2e28c03d32a3db285f
Tigran542 <tikogrig7@gmail.com>
2023-08-18 16:28:57 +0400

    Basic script

--- Assets/Scripts/Basics.cs
+++ Assets/Scripts/Basics.cs
@@ -2,34 +2,11 @@ using UnityEngine;
 
 public class Basics : MonoBehaviour
 {
-    private void Awake()
-    {
-        Debug.Log("Awake");
-    }
+
+    public GameObject obj;
+
     private void Start()
     {
-        Debug.Log("Start");
-    }
-
-    private void Update()
-    {
-        Debug.Log("Update");
-    }
-
-    private void LateUpdate()
-    {
-        Debug.Log("LateUpdate");
-    }
-    private void FixedUpdate()
-    {
-        Debug.Log("FixedUpdate");
-    }
-    private void OnDestroy()
-    {
-        Debug.Log("OnDestroy");
-    }
-    private void OnEnable()
-    {
-        Debug.Log("OnEnable");
+        obj.SetActive(false);
     }
 }
