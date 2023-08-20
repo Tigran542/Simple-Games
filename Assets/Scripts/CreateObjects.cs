a6acc75413af17d02fdfc91da238d9ef8bec0b9b
Tigran542 <tikogrig7@gmail.com>
2023-08-20 19:02:21 +0400

    RandomNumber()

--- Assets/Scripts/CreateObjects.cs
+++ Assets/Scripts/CreateObjects.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class CreateObjects : MonoBehaviour
 {
 
-    public GameObject obj;
+    public GameObject[] obj;
 
     private void Start()
     {
-        Instantiate(obj, new Vector3(0, 5, 0), Quaternion.Euler(12f, -15f, 40f));
+        for (int i = 0; i < 5; i++)
+        {
+            // GameObject newObject = Instantiate(obj, new Vector3(0, 5, 0), Quaternion.Euler(12f, -15f, 40f)) as GameObject;
+            //  newObject.GetComponent<Transform>().position = new Vector3(5, 5, 0);
+            Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(RandomNumber(), RandomNumber(), RandomNumber()), Quaternion.Euler(RandomNumber(), -15f, 40f));
+
+        }
     }
 
+    private int RandomNumber()
+    {
+        return Random.Range(0, obj.Length);
+    }
 }
