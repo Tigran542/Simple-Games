6eccbbab54fa483513661c21ab796bcc43ae988a
Tigran542 <tikogrig7@gmail.com>
2023-08-26 17:41:33 +0400

    IEnumerator and Coroutine

--- Assets/Scripts/CreateObjects.cs
+++ Assets/Scripts/CreateObjects.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
 public class CreateObjects : MonoBehaviour
 {
 
     public GameObject[] obj;
-
     private void Start()
+    {
+        // Invoke("Create", 2f);
+        StartCoroutine(Create3dObjects());
+    }
+
+    private void Create()
     {
         for (int i = 0; i < 5; i++)
         {
             // GameObject newObject = Instantiate(obj, new Vector3(0, 5, 0), Quaternion.Euler(12f, -15f, 40f)) as GameObject;
             //  newObject.GetComponent<Transform>().position = new Vector3(5, 5, 0);
-            Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(RandomNumber(), RandomNumber(), RandomNumber()), Quaternion.Euler(RandomNumber(), -15f, 40f));
+            Instantiate(obj[UnityEngine.Random.Range(0, obj.Length)], new Vector3(RandomNumber(), RandomNumber(), RandomNumber()), Quaternion.Euler(RandomNumber(), -15f, 40f));
 
         }
     }
 
     private int RandomNumber()
     {
-        return Random.Range(0, obj.Length);
+        return UnityEngine.Random.Range(0, obj.Length);
+    }
+
+    private IEnumerator Create3dObjects()
+    {
+        yield return new WaitForSeconds(3f);
+        Create();
+        Debug.Log("Lebiga");
     }
 }
